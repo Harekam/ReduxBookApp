7380c239caea8dcc628cd94e2e07f81f39707471
Harekam Singh <harekam.singh@COMVIVA.COM>
2017-07-13 20:46:31 +0530

    first commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,5 @@
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+import {combineReducers} from 'redux';
+import BooksReducer from './reducers_books';
+const rootReducer = combineReducers({books: BooksReducer});
 
 export default rootReducer;
---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,18 @@
+export default function () {
+    return [{
+            title: "JS"
+        },
+        {
+            title: "JS1"
+        },
+        {
+            title: "JS2"
+        },
+        {
+            title: "JS3"
+        },
+        {
+            title: "JS4"
+        }
+    ];
+}
